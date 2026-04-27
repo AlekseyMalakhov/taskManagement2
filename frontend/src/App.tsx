5236bce67cc7738579baae9f13ad1a21a9ed9e9c
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2026-04-27 15:35:28 +0300

    add react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
 
 function App() {
-
   return (
-    <div>Hi all</div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
   )
 }
 
---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main>
+      <h1>Task Manager</h1>
+      <p>Your tasks will appear here.</p>
+    </main>
+  )
+}
